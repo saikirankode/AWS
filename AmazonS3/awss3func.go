fa9f262ea5fe14e90d7a306ecbc528544a5894ab
Sai Kode <saikirankode@gmail.com>
2017-10-02 14:50:33 -0400

    all func in one file

--- AmazonS3/awss3func.go
+++ AmazonS3/awss3func.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"net/http"
 	"os"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 type file struct {
@@ -43,4 +44,75 @@ func uploadobject(res http.ResponseWriter, req *http.Request) {
 		fmt.Println("upload error", err)
 	}
 	fmt.Println(up, "done uploadong")
+}
+
+func Listobject(res http.ResponseWriter, req *http.Request) {
+	var newfile file
+	params := req.URL.Query()
+	newfile.Bucketname=params["bucketname"][0]
+	accessid := os.Getenv("s3accessid")
+	accesskey := os.Getenv("secretacceskey")
+	token := ""
+	cred := credentials.NewStaticCredentials(accessid, accesskey, token)
+	config := aws.NewConfig()
+	config.Credentials = cred
+	config.Region = aws.String("us-east-2")
+	svc := s3.New(session.New(config))
+	buckname := newfile.Bucketname
+	list, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(buckname)})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(list)
+}
+
+func Downloadobject(res http.ResponseWriter, req *http.Request) {
+	var newfile file
+	params := req.URL.Query()
+	newfile.Bucketname=params["bucketname"][0]
+	newfile.Objectname=params["objectname"][0]
+	fmt.Fprintln(res,newfile)
+	accessid := os.Getenv("s3accessid")
+	accesskey := os.Getenv("secretacceskey")
+	token := ""
+	cred := credentials.NewStaticCredentials(accessid, accesskey, token)
+	config := aws.NewConfig()
+	config.Credentials = cred
+	config.Region = aws.String("us-east-2")
+	sess := session.Must(session.NewSession(config))
+	downloader := s3manager.NewDownloader(sess)
+	f, err := os.Create("uploadfile")
+	if err != nil {
+		fmt.Println("cannot create a file", err)
+	}
+	buckname := newfile.Bucketname
+	objname := newfile.Objectname
+	dow, err := downloader.Download(f, &s3.GetObjectInput{Bucket: aws.String(buckname), Key: aws.String(objname),})
+	if err != nil {
+		fmt.Println("Error downloading File", err)
+	}
+	fmt.Println("new uploadfile download", dow)
+}
+
+func Deleteobject(res http.ResponseWriter, req *http.Request) {
+	var newfile file
+	params := req.URL.Query()
+	newfile.Bucketname=params["bucketname"][0]
+	newfile.Objectname=params["objectname"][0]
+	fmt.Fprintln(res,newfile)
+	accessid := os.Getenv("s3accessid")
+	accesskey := os.Getenv("secretacceskey")
+	token := ""
+	cred := credentials.NewStaticCredentials(accessid, accesskey, token)
+	config := aws.NewConfig()
+	config.Credentials = cred
+	config.Region = aws.String("us-east-2")
+	svc := s3.New((session.New(config)))
+	buckname := newfile.Bucketname
+	objname := newfile.Objectname
+	deleteout, err := svc.DeleteObject(&s3.DeleteObjectInput{Bucket: aws.String(buckname), Key: aws.String(objname)})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(res,deleteout)
 }
--- AmazonS3/main.go
+++ AmazonS3/main.go
@@ -10,6 +10,9 @@ func main() {
 	//Uploadobject("uploadfile","awsassign","now")
 	router := mux.NewRouter()
 	router.HandleFunc("/uploadobj", uploadobject).Methods("PUT") // (/uploadobject?filename=&bucketname=&objectname)
+	router.HandleFunc("/listobj", Listobject).Methods("GET")     //(/listobj?bucketname=)
+	router.HandleFunc("/downloadobj", Downloadobject).Methods("GET") //(/downloadobj?bucketname=&objectname=)
+	router.HandleFunc("/deleteobj", Deleteobject).Methods("DELETE")  //(/deleteobj?bucketname=&objectname=)
 	log.Fatal(http.ListenAndServe(":8081", router))
 
 }
